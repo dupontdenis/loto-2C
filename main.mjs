747bf84a59aceed1a150f6cd6ac9f5c93fd5352a
DENIS DUPONT <denis.dupont@univ-evry.fr>
2025-10-21 15:50:24 +0200

    first commit

---
+++ main.mjs
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+const choices = document.querySelector(".choices");
+
+const handleSelect = function (event) {
+  const targetElement = event.target; // span ou li ou ul
+
+  if (targetElement.closest("div")) {
+    const selectedDiv = targetElement.closest(".container > div");
+    // Toggle selected class
+    selectedDiv.classList.toggle("selected");
+    const selectedElements = document.querySelectorAll(
+      ".container > div.selected"
+    );
+    // Clear previous choices
+    choices.innerHTML = "";
+    // Add selected choices to the choices container
+    selectedElements.forEach((element) => {
+      //clone the selected element
+      const clone = element.cloneNode(true);
+      choices.appendChild(clone);
+    });
+  }
+};
+
+container.addEventListener("click", handleSelect, false);
